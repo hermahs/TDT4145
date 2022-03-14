23453cb9225feaae30359c6cf546d8de4acb655b
simeonc01 <101096082+simeonc01@users.noreply.github.com>
2022-03-14 17:00:18 +0100

    Oppdaterte SQL-en
    Endret på noen klasser og navn slik at de tilsvarer tabellene vi har laget

--- Database.sql
+++ Database.sql
@@ -1,36 +1,5 @@
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE Art (
-    	ArtNavn TEXT PRIMARY KEY NOT NULL
-);
-
-CREATE TABLE ForedlingsMetode (
-    	Navn TEXT PRIMARY KEY NOT NULL,
-	Beskrivelse TEXT UNIQUE
-);
-
-CREATE TABLE Region (
-	Region TEXT NOT NULL,
-	Land TEXT NOT NULL,
-	PRIMARY KEY (Region, Land)
-);
-
-CREATE TABLE Gard (
-    	GardID INTEGER PRIMARY KEY NOT NULL,
-	Navn TEXT NOT NULL,
-    	MOH INTEGER NOT NULL,
-    	Region TEXT NOT NULL,
-	Land TEXT NOT NULL,
-    	FOREIGN KEY (Region, Land)
-        	REFERENCES Region (Region, Land)
-			ON DELETE RESTRICT
-);
-
-CREATE TABLE KaffeBrenneri (
-    	BrenneriID INTEGER PRIMARY KEY NOT NULL,
-    	Navn TEXT NOT NULL
-);
-
 CREATE TABLE Bruker (
     	Epost TEXT PRIMARY KEY NOT NULL,
     	Fornavn TEXT NOT NULL,
@@ -38,127 +7,127 @@ CREATE TABLE Bruker (
     	Passord TEXT NOT NULL
 );
 
-CREATE TABLE FerdigBrentKaffe (
-    	Navn TEXT NOT NULL,
-    	Dato TEXT NOT NULL,
-    	Beksrivelse TEXT NOT NULL,
-    	Kilopris INTEGER NOT NULL ,
+CREATE TABLE Kaffesmaking (
+    	Epost TEXT NOT NULL,
+	KaffeNavn TEXT NOT NULL,
     	BrenneriID INTEGER NOT NULL,
-	PartiID INTEGER NOT NULL,
-	PRIMARY KEY (Navn, BrenneriID),
-	FOREIGN KEY (BrenneriID)
-        	REFERENCES KaffeBrenneri (BrenneriID)
-		ON DELETE CASCADE
-		ON UPDATE NO ACTION,
-	FOREIGN KEY (PartiID)
-		REFERENCES KaffeParti (PartiID)
-		ON DELETE CASCADE
-		ON UPDATE NO ACTION
-);
-
-CREATE TABLE KaffeSmaking (
-    	epost TEXT NOT NULL,
-    	BrentKaffeID INTEGER NOT NULL,
     	Notat TEXT,
   	Poeng INTEGER NOT NULL,
     	Dato TEXT,
-    	PRIMARY KEY (epost, BrentKaffeID),
-    	FOREIGN KEY (epost)
-        	REFERENCES Bruker (epost)
+	
+    	PRIMARY KEY (Epost, BrenneriID),
+    	FOREIGN KEY (Epost)
+        	REFERENCES Bruker (Epost)
             		ON DELETE CASCADE
             		ON UPDATE NO ACTION,
-    	FOREIGN KEY (BrentKaffeID)
-        	REFERENCES FerdigBrentKaffe (BrentKaffeID)
+    	FOREIGN KEY (KaffeNavn, BrenneriID)
+        	REFERENCES FerdigbrentKaffe (Navn)
             		ON DELETE CASCADE
             		ON UPDATE NO ACTION
 );
 
-CREATE TABLE KaffeBonner (
-    	KaffeBonneID INTEGER PRIMARY KEY NOT NULL,
+CREATE TABLE FerdigbrentKaffe (
+    	Navn TEXT NOT NULL,
+	BrenneriID INTEGER NOT NULL,
+	Brenningsgrad TEXT NOT NULL,
+    	Dato TEXT NOT NULL,
+    	Beksrivelse TEXT NOT NULL,
+    	KiloprisNOK INTEGER NOT NULL ,
+	PartiID INTEGER NOT NULL,
+	
+	PRIMARY KEY (Navn, BrenneriID),
+	FOREIGN KEY (PartiID)
+		REFERENCES Kaffeparti (PartiID)
+		ON DELETE CASCADE
+		ON UPDATE NO ACTION,
+	FOREIGN KEY (BrenneriID)
+        	REFERENCES Kaffebrenneri (ID)
+            		ON DELETE CASCADE
+            		ON UPDATE NO ACTION
+);
+
+CREATE TABLE Kaffebrenneri (
+    	ID INTEGER PRIMARY KEY NOT NULL,
     	Navn TEXT NOT NULL
 );
 
-CREATE TABLE KaffeParti (
-    	PartiID INTEGER PRIMARY KEY NOT NULL,
+CREATE TABLE Kaffeparti (
+    	ID INTEGER PRIMARY KEY NOT NULL,
     	InnhostningAr INTEGER NOT NULL,
-    	Gard INTEGER NOT NULL,
-    	ForedlingsMetode TEXT NOT NULL,
-    	FOREIGN KEY (ForedlingsMetode)
-        	REFERENCES ForedlingsMetode (Navn)
+	KiloprisUSD INTEGER NOT NULL,
+    	GardID INTEGER NOT NULL,
+    	ForedlingsNavn TEXT NOT NULL,
+	
+    	FOREIGN KEY (ForedlingsNavn)
+        	REFERENCES Foredlingsmetode (Navn)
 			ON DELETE RESTRICT,
-    	FOREIGN KEY (Gard)
+    	FOREIGN KEY (GardID)
         	REFERENCES Gard (GardID)
 			ON DELETE RESTRICT
 );
 
-CREATE TABLE Kjoper (
-    	BrenneriID INTEGER NOT NULL,
-    	PartiId INTEGER NOT NULL,
-    	Kilopris INTEGER NOT NULL,
-    	PRIMARY KEY (BrenneriID, PartiID),
-    	FOREIGN KEY (BrenneriID)
-        	REFERENCES KaffeBrenneri (BrenneriID)
-            		ON DELETE CASCADE
-        	    	ON UPDATE NO ACTION,
-    	FOREIGN KEY (PartiID)
-        	REFERENCES KaffeParti (PartiID)
-            		ON DELETE CASCADE
-            		ON UPDATE NO ACTIOn
+CREATE TABLE Art (
+    	Navn TEXT PRIMARY KEY NOT NULL
 );
 
-CREATE TABLE TilhorerArt (
+CREATE TABLE KaffebonneType (
+    	Navn INTEGER PRIMARY KEY NOT NULL,
     	ArtNavn TEXT NOT NULL,
-    	KaffeBonneID INTEGER NOT NULL,
-    	PRIMARY KEY (ArtNavn, KaffeBonneID),
-    	FOREIGN KEY (ArtNavn)
-        	REFERENCES Art (ArtNavn)
+	
+	FOREIGN KEY (ArtNavn)
+        	REFERENCES Art (Navn)		
+);
+
+CREATE TABLE BestarAv (
+    	KaffebonneNavn INTEGER NOT NULL,
+    	PartiID INTEGER NOT NULL,
+	
+    	PRIMARY KEY (KaffebonneNavn, PartiID),
+    	FOREIGN KEY (KaffebonneNavn)
+        	REFERENCES KaffebonneType (Navn)
             		ON DELETE CASCADE
             		ON UPDATE NO ACTION,
-    	FOREIGN KEY (KaffeBonneID)
-        	REFERENCES KaffeBonner (KaffeBonneID)
+    	FOREIGN KEY (PartiID)
+        	REFERENCES Kaffeparti (PartiID)
             		ON DELETE CASCADE
             		ON UPDATE NO ACTION
 );
 
-CREATE TABLE BonneForedles (
-    	KaffeBonneID INTEGER NOT NULL,
-    	Navn TEXT NOT NULL,
-    	PRIMARY KEY (KaffeBonneID, Navn),
-    	FOREIGN KEY (KaffeBonneID)
-        	REFERENCES KaffeBonner (KaffeBonneID)
-            		ON DELETE CASCADE
-            		ON UPDATE NO ACTION,
-    	FOREIGN KEY (Navn)
-        	REFERENCES ForedlingsMetode (Navn)
-            		ON DELETE CASCADE
-            		ON UPDATE NO ACTION
+CREATE TABLE Foredlingsmetode (
+    	Navn TEXT PRIMARY KEY NOT NULL,
+	Beskrivelse TEXT UNIQUE
 );
 
-CREATE TABLE DyrketAv (
-    	KaffeBonneID INTEGER NOT NULL,
+CREATE TABLE Gard (
+    	ID INTEGER PRIMARY KEY NOT NULL,
+	Navn TEXT NOT NULL,
+    	MeterOverHavet INTEGER NOT NULL,
+    	Region TEXT NOT NULL,
+	Land TEXT NOT NULL,
+	
+    	FOREIGN KEY (Region, Land)
+        	REFERENCES Region (Region, Land)
+			ON DELETE RESTRICT
+);
+
+CREATE TABLE DyrkesAv (
+    	KaffebonneNavn INTEGER NOT NULL,
     	GardID INTEGER NOT NULL,
-    	PRIMARY KEY (KaffeBonneID, GardID),
-    	FOREIGN KEY (KaffeBonneID)
-        	REFERENCES KaffeBonner (KaffeBonneID)
+	
+    	PRIMARY KEY (KaffebonneNavn, GardID),
+    	FOREIGN KEY (KaffebonneNavn)
+        	REFERENCES KaffebonneType (Navn)
             		ON DELETE CASCADE
             		ON UPDATE NO ACTION,
     	FOREIGN KEY (GardID)
-        	REFERENCES Gard (GardID)
+        	REFERENCES Gard (ID)
             		ON DELETE CASCADE
             		ON UPDATE NO ACTION
 );
 
-CREATE TABLE Har (
-    	KaffeBonneID INTEGER NOT NULL,
-    	PartiID INTEGER NOT NULL,
-    	PRIMARY KEY (KaffeBonneID, PartiID),
-    	FOREIGN KEY (KaffeBonneID)
-        	REFERENCES KaffeBonner (KaffeBonneID)
-            		ON DELETE CASCADE
-            		ON UPDATE NO ACTION,
-    	FOREIGN KEY (PartiID)
-        	REFERENCES KaffeParti (PartiID)
-            		ON DELETE CASCADE
-            		ON UPDATE NO ACTION
+CREATE TABLE Region (
+	Region TEXT NOT NULL,
+	Land TEXT NOT NULL,
+	
+	PRIMARY KEY (Region, Land)
 );
-		
